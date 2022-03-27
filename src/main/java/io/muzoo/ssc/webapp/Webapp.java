84f584a4ef8cae009cfb0ea99d17d0206d3821ff
Yuttasart Viratpan <yuttasartwoodsiam@gmail.com>
2022-03-27 21:32:37 +0700

    Wepapp deployed settings ver 1

--- src/main/java/io/muzoo/ssc/webapp/Webapp.java
+++ src/main/java/io/muzoo/ssc/webapp/Webapp.java
@@ -18,7 +18,7 @@ public class Webapp {
         File docBase = new File("src/main/webapp/");
         docBase.mkdirs();
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8082);
+        tomcat.setPort(80);
 
         SecurityService securityService = new SecurityService();
         securityService.setUserService(UserService.getInstance());
